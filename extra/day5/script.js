be9a1da3ddd227004fc12bca4214db754c7c506b
manikanth <kanalamanikanthreddy18@gmail.com>
2026-04-14 09:48:23 +0530

    commit

--- extra/day5/script.js
+++ extra/day5/script.js
@@ -1,5 +1,12 @@
-console.log(`hello world`);
-let name ="riya";
-let age =25;
-console.log(`my name is ${name}. and my age is ${age}`);
-
+// // console.log(`hello world`);
+// // let name ="riya";
+// // let age =25;
+// // console.log(`my name is ${name}. and my age is ${age}`);
+// let mani=document.getElementById("mani");
+// mani.innerText="i hate u";
+//  mani.innerHTML="<h2>u hate me</h2>"
+let boxes =document.getElementsByClassName("id1");
+boxes[0].innerHTML="hi ra chitti";
+let hero =document.getElementsByClassName("hero");
+hero[0].innerHTML="hero";
+hero[1].innerHTML="<h1>villan</h1>"
