d408279635f89090885400f9c017b9d4b94dd53f
manikanth <kanalamanikanthreddy18@gmail.com>
2026-04-17 09:16:29 +0530

    first commit

--- day 4/script.js
+++ day 4/script.js
@@ -1,23 +1,23 @@
-// const myFun = () => {
-//     console.log("hello sir");
-// }
-// const changetext =() =>{
-//     let display = document.getElementById("hiii")
-//     display.innerHTML="<h1>Hello sir </p>"
-//     display.style.color ="blue"
+// // const myFun = () => {
+// //     console.log("hello sir");
+// // }
+// // const changetext =() =>{
+// //     let display = document.getElementById("hiii")
+// //     display.innerHTML="<h1>Hello sir </p>"
+// //     display.style.color ="blue"
     
-// }
-// const validateForm =(event)=>{
+// // }
+// // const validateForm =(event)=>{
+// //     event.preventDefault()
+// // }
+// //userinput
+// //array.push()
+// //array-print like list
+// const userdata=[];
+// const addtask =(event) =.{
 //     event.preventDefault()
+//     let userinput =event.target.userinput
+//     let display=document.getElementById("display")
+//     userdata.push(userinput.value)
+//     display.innerHTML=userdata
 // }
-//userinput
-//array.push()
-//array-print like list
-const userdata=[];
-const addtask =(event) =.{
-    event.preventDefault()
-    let userinput =event.target.userinput
-    let display=document.getElementById("display")
-    userdata.push(userinput.value)
-    display.innerHTML=userdata
-}
